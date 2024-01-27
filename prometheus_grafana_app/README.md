ca2620407b13611d5b0983d75c1271ab6b45061b
Alexandr Meshkov <alex.meh96@mail.ru>
2024-01-27 23:27:59 +0300

    prometheus_grafana_app: created

---
+++ prometheus_grafana_app/README.md
@@ -0,0 +1,1 @@
+Пример использования prometheus и grafana
---
+++ prometheus_grafana_app/app/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
+)
+
+type Device struct {
+	Id       int    `json:"id"`
+	Mac      string `json:"mac"`
+	Firmware string `json:"firmware"`
+}
+
+type metrics struct {
+	devices prometheus.Gauge
+	info    *prometheus.GaugeVec
+}
+
+func NewMetrics(req prometheus.Registerer) *metrics {
+	m := &metrics{
+		devices: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: "myapp",
+			Name:      "connected_devices",
+			Help:      "Number of currently connected devices",
+		}),
+		info: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "myapp",
+			Name:      "info",
+			Help:      "Information about the My App environment.",
+		}, []string{"version"}),
+	}
+	req.MustRegister(m.devices, m.info)
+	return m
+}
+
+var dvs []Device
+var version string
+
+func init() {
+	version = "2.10.5"
+
+	dvs = []Device{
+		{1, "5F-33-CC-1F-43-82", "2.1.6"},
+		{2, "EF-2B-C4-F5-D6-34", "2.1.6"},
+	}
+}
+
+func main() {
+	reg := prometheus.NewRegistry()
+	m := NewMetrics(reg)
+
+	m.devices.Set(float64(len(dvs)))
+	m.info.With(prometheus.Labels{"version": version}).Set(1)
+
+	dMux := http.NewServeMux()
+	rdh := registerDeviceHandler{metrics: m}
+	dMux.Handle("/devices", rdh)
+
+	pMux := http.NewServeMux()
+	promHandler := promhttp.HandlerFor(reg, promhttp.HandlerOpts{})
+	pMux.Handle("/metrics", promHandler)
+
+	go func() {
+		log.Fatal(http.ListenAndServe(":8080", dMux))
+	}()
+
+	go func() {
+		log.Fatal(http.ListenAndServe(":8081", pMux))
+	}()
+
+	select {}
+}
+
+type registerDeviceHandler struct {
+	metrics *metrics
+}
+
+func (rdh registerDeviceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		getDevices(w, r)
+	case "POST":
+		createDevice(w, r, rdh.metrics)
+	default:
+		w.Header().Set("Allow", "GET, POST")
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func getDevices(w http.ResponseWriter, r *http.Request) {
+	b, err := json.Marshal(dvs)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func createDevice(w http.ResponseWriter, r *http.Request, m *metrics) {
+	var dv Device
+
+	err := json.NewDecoder(r.Body).Decode(&dv)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	dvs = append(dvs, dv)
+	m.devices.Set(float64(len(dvs)))
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Device created!"))
+}
